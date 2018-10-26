db9b3028d3ec4c44e2a88f5177f2341a783e6045
Nicolas Vincent <nvince01@utbm.fr>
2018-10-26 14:47:56 +0200

    Entity creation

---
+++ src/main/java/org/vinksel/ultimateprojectofdoom3000lesdeuxtours/entity/Client.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.vinksel.ultimateprojectofdoom3000lesdeuxtours.entity;
+
+/**
+ *
+ * @author nvince01
+ */
+public class Client {
+    
+}
