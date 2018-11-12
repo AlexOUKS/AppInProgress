4d8e332f4608b0ec06df2c0241630397b9b941ce
Alexis <ouksel.alexis@hotmail.fr>
2018-11-12 13:45:45 +0100

    enfin :)

--- API/src/main/java/org/vinksel/ultimateprojectofdoom3000lesdeuxtours/controllers/SessionController.java
+++ API/src/main/java/org/vinksel/ultimateprojectofdoom3000lesdeuxtours/controllers/SessionController.java
@@ -12,25 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.vinksel.ultimateprojectofdoom3000lesdeuxtours.entities.Course;
-import org.vinksel.ultimateprojectofdoom3000lesdeuxtours.entities.Session;
 
 @RestController
-<<<<<<< HEAD:API/src/main/java/org/vinksel/ultimateprojectofdoom3000lesdeuxtours/controller/CourseController.java
-public class CourseController {
+
+public class SessionController {
 	@RequestMapping("/courses")
 	public ArrayList<Course> courses(@RequestBody String body){		
 		SessionFactory sf = new Configuration().configure().buildSessionFactory();
 		Session session = sf.openSession();
 		Query qry = session.createQuery("from course c");
 		List l =qry.list();
-=======
-public class SessionController {
-	@RequestMapping("/sessions") 
-	public ArrayList<Session> sessions(@RequestParam(value="code", defaultValue="all") String code, 
-									 @RequestParam(value="name", defaultValue="all") String name,
-									 @RequestParam(value="id", defaultValue="all") String id){
->>>>>>> 2acf67bd2c9b6149e58c9728831ef17097057127:API/src/main/java/org/vinksel/ultimateprojectofdoom3000lesdeuxtours/controllers/SessionController.java
 		return null;
 	}
-
+	
 }
