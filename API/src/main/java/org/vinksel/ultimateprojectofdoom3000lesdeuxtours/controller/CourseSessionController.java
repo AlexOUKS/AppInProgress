ef9d0b10b015db9c983343207d609f860c50a4ac
Alexis <ouksel.alexis@hotmail.fr>
2018-11-12 13:37:30 +0100

    1st step

--- API/src/main/java/org/vinksel/ultimateprojectofdoom3000lesdeuxtours/controller/CourseSessionController.java
+++ API/src/main/java/org/vinksel/ultimateprojectofdoom3000lesdeuxtours/controller/CourseSessionController.java
@@ -1,8 +1,19 @@
 package org.vinksel.ultimateprojectofdoom3000lesdeuxtours.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.vinksel.ultimateprojectofdoom3000lesdeuxtours.entity.Course;
+import org.vinksel.ultimateprojectofdoom3000lesdeuxtours.entity.CourseSession;
 
 @RestController
 public class CourseSessionController {
-
+	
+	@PostMapping("/subscribeSession")
+	public CourseSession courses(@RequestBody CourseSession newEmployee){		
+		
+		return null;
+	}
 }
