acda75031cefaa286db5a94b42b86c9b6aa1d207
Alexis <ouksel.alexis@hotmail.fr>
2018-12-18 15:28:12 +0100

    ok

---
+++ app/src/main/newSession/NewSession.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader, Input, Row, Col, Progress } from 'reactstrap';
+import './NewSession.css';
+
+class NewSession extends Component {  
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+    
+    render() {
+        return ("ici")
+    }
+}
+
+export default NewSession;
